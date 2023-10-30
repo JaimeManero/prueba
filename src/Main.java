93da0fc7ddf205b033420b2db24dc483751ad399
ik012982i9 <mbelvermi23dw@ikzubirimanteo.com>
2023-10-30 12:51:35 +0100

    cambios

--- src/Main.java
+++ src/Main.java
@@ -2,5 +2,9 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
         System.out.println("Menos 7");
+        System.out.println("Decepcionante");
+        System.out.println(":(");
+        System.out.println("Lentejas pa comer");
+
     }
 }
